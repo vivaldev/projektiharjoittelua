2bf0d7f7f775f276d2a9aeaa8c62a6ee740e1156
vivaldev <109537087+vivaldev@users.noreply.github.com>
2022-11-01 22:06:55 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function App() {
+	const axios = require('axios').default;
+
+	return (
+		<div className="App">
+			<h1>Hello CodeSandbox</h1>
+			<h2>Start editing to see some magic happen!</h2>
+		</div>
+	);
+}
